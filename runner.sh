85a6c3cb0fbe708326d2ee2a3dff6a8741f47d32
Karan Bania <101618474+karannb@users.noreply.github.com>
2024-07-04 23:55:54 +0200

    updated runner file and .gitignore

--- runner.sh
+++ runner.sh
@@ -10,10 +10,9 @@
 
 python3 main.py --log_every 25 --batch_size 64 --epochs 500 --lr 3e-5 \
  --weight_decay 0 --folds 10 --eval_every 50 --save_every 100 \
- --patience 20 --seed 40 --layer_num 7 --task regression --log_dir results/fivesixseven_cls \
+ --patience 20 --seed 40 --layer_num 7 --task regression --log_dir results \
  --ckpt_path ckpts/st_gcn.kinetics.pt --non_overlapping
 
-# Can also start with nohup
 # additional args - 
 # --with_tracking
 # --use_mlp
